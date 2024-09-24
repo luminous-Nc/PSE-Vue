ce099095a2533b99cff07e2cc1267a4fc8d1b968
cheng <chengniu@tamu.edu>
2024-09-24 12:49:28 -0500

    Learning View

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -9,7 +9,7 @@
                 <ul class="font-medium flex p-0 border-gray-100 rounded-lg  flex-row space-x-8 rtl:space-x-reverse mt-0 border-0 bg-white dark:bg-gray-900 dark:border-gray-700">
                     <li>
                         <a href="/"
-                              class="block py-2 px-3 rounded bg-transparent text-blue-700 p-0 dark:text-blue-500">Home</a>
+                              class="block py-2 px-3 rounded bg-transparent text-blue-700 p-0 dark:text-blue-500">Learn</a>
                     </li>
                     <li>
                         <a href="/about"
---
+++ src/views/LearningView.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="min-h-screen flex-col items-center p-20 pl-2 pr-2 space-y-24">
+    <div class="p-6 w-full h-full bg-white border border-gray-300 rounded-lg shadow">
+      <div class="text-center py-8">
+        <h1 class="text-5xl font-bold mb-6">The Learning Styles Quiz</h1>
+        <div class="border-t-4 border-blue-600 w-6/12 mx-auto mb-6"></div>
+        <p class="text-gray-600 max-w-3xl mx-auto text-justify">
+          Individuals perceive and process information in different ways, known as their <b>learning styles</b>. The understanding of students' natural preference will facilitate faster learning and reduce frustration and failures.
+        </p>
+        <p class="text-gray-600 max-w-3xl mx-auto text-justify">
+
+        </p>
+      </div>
+    </div>
+
+
+  </main>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+
+</style>
